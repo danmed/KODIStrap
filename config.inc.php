7778576d534333e5ce6aba2d441b58ede663ee67
danmed <danmed@gmail.com>
2017-11-03 16:34:38 +0000

    Create config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,16 @@
+<?PHP
+# MYSQL information
+$server 	= 	'192.168.1.100';
+$database 	= 	"myvideos107";
+$username 	= 	'kodi';
+$password 	= 	'kodi';
+# API key from tmdb.com
+$apikey 	= 	'';
+# API key from tvdb.com
+$tvdbkey  =   '';
+# Internal IP's for "frontroom" and "bedroom" XBMC instances including port numbers
+$xbmc1 		= 	"192.168.1.26:8080";
+$xbmc1label	=	"BR";
+$xbmc2 		= 	"192.168.1.25:8080";
+$xbmc2label	=	"FR";
+?> 
