76eabee5e5ba45d25f307547ea18a354f1fab936
danmed <danmed@gmail.com>
2017-11-04 14:56:13 +0000

    Update moviemodal.php

--- moviemodal.php
+++ moviemodal.php
@@ -9,7 +9,7 @@
         <h4 class="modal-title">About PStatus</h4>
       </div>
       <div class="modal-body">
-        <p><?PHP echo $movieid; ?></p>
+        <p><?PHP echo $showmovie; ?></p>
         <p><a href="https://github.com/danmed/PStatus">Github</a></p>
       </div>
       <div class="modal-footer">
