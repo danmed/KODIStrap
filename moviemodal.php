96aba50c8cdee9bbdc53bbadaef5c9153a03fc0f
danmed <danmed@gmail.com>
2017-11-04 13:34:41 +0000

    Create moviemodal.php

---
+++ moviemodal.php
@@ -0,0 +1,21 @@
+<!-- Modal -->
+<div id="aboutmodal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">About PStatus</h4>
+      </div>
+      <div class="modal-body">
+        <p>Written by Dan Medhurst</p>
+        <p><a href="https://github.com/danmed/PStatus">Github</a></p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
