dec5d2e3badd101c2546d22be182ed002e02e1e1
danmed <danmed@gmail.com>
2017-11-04 14:57:01 +0000

    Update index.php

--- index.php
+++ index.php
@@ -60,7 +60,7 @@
 	     ?>
    <script type='text/javascript'>
     $(document).ready(function(){
-    $('#movieodal').modal('show');
+    $('#moviemodal').modal('show');
     });
     </script>
 <?php
